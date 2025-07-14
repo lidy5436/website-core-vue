461f32fab494145c61bbea625ea76b8609d83d2b
lidy <731729142@qq.com>
2025-07-14 23:02:45 +0800

    【新增】基础弹窗模板

--- src/components/basicDialog/index.vue
+++ src/components/basicDialog/index.vue
@@ -1,10 +1,44 @@
-<script setup>
-
-</script>
-
 <template>
+  <el-dialog
+      title="弹窗标题"
+      draggable
+      destroy-on-close
+      v-model="dialogVisible">
+    <div>弹窗内容</div>
+    <template #footer>
+      <div>
+        <el-button @click="handleCancel">取 消</el-button>
+        <el-button type="primary" @click="handleSubmit">确 定</el-button>
+      </div>
+    </template>
+  </el-dialog>
 
 </template>
+
+<script setup>
+import {ref} from "vue";
+
+// 弹窗打开标识
+const dialogVisible = ref(false);
+// 打开方法
+const open = () => dialogVisible.value = true;
+// 关闭方法
+const close = () => dialogVisible.value = false;
+// 取消方法
+const handleCancel = () => {
+  close();
+}
+// 确认方法
+const handleSubmit = () => {
+  close()
+}
+
+defineExpose({
+  open,
+  close
+})
+
+</script>
 
 <style scoped>
 
--- src/view/home/index.vue
+++ src/view/home/index.vue
@@ -1,10 +1,25 @@
 <template>
-  首页
+  <div>
+    <el-button type="primary" @click="handleOpenDialog">打开弹窗</el-button>
+  </div>
+
+  <!-- 基础弹窗 -->
+  <basic-dialog ref="basicDialogRef"/>
 
 </template>
 
 <script setup>
+import {ref} from "vue";
+import BasicDialog from '@/components/basicDialog/index.vue'
 
+// 基础弹窗
+const basicDialogRef = ref(undefined)
+
+// 打开弹窗
+const handleOpenDialog = () => {
+  basicDialogRef.value.open()
+
+}
 </script>
 
 <style scoped>
